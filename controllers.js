a34a7f382cb1052ea2f63a67d23fa808b5b86e7b
Michael Trang <michael.trang@hotmail.com>
2015-02-27 23:33:37 +1100

    Refactoring to use custom directive weatherService

--- controllers.js
+++ controllers.js
@@ -15,17 +15,19 @@ angular.module('weatherApp').controller('homeController', ['$scope', '$location'
     
 }]);
 
-angular.module('weatherApp').controller('forecastController',['$scope','$resource','$routeParams','cityService', function ($scope, $resource, $routeParams, cityService) {
+angular.module('weatherApp').controller('forecastController',['$scope','$resource','$routeParams','cityService', 'weatherService', function ($scope, $resource, $routeParams, cityService, weatherService) {
     this.city = cityService.city;
     
     this.days = $routeParams.days || '5'; //default to 5 days.
     
-    this.weatherAPI = $resource(
-        "http://api.openweathermap.org/data/2.5/forecast/daily", 
-        {callback: "JSON_CALLBACK"},
-        { get: {method: "JSONP"}});
+    this.weatherResult = weatherService.GetWeather(this.city, this.days);
     
-    this.weatherResult = this.weatherAPI.get({ q: this.city, cnt: this.days });
+//    this.weatherAPI = $resource(
+//        "http://api.openweathermap.org/data/2.5/forecast/daily", 
+//        {callback: "JSON_CALLBACK"},
+//        { get: {method: "JSONP"}});
+//    
+//    this.weatherResult = this.weatherAPI.get({ q: this.city, cnt: this.days });
     
     this.convertToFahrenheit = function(degK) {
         return Math.round( 1.8 * (degK - 273.15) + 32);
--- services.js
+++ services.js
@@ -1,4 +1,17 @@
 // Service component
-angular.module('weatherApp').service('cityService', function() {
+angular.module('weatherApp').service('cityService', function () {
     this.city = "Sydney, NSW";
 });
+
+angular.module('weatherApp').service('weatherService',['$resource', function ($resource) {
+    
+    this.GetWeather = function (city, days) {
+        
+        var weatherAPI = $resource(
+                                "http://api.openweathermap.org/data/2.5/forecast/daily", 
+                                {callback: "JSON_CALLBACK"},
+                                { get: {method: "JSONP"}});
+
+        return weatherAPI.get({ q: city, cnt: days });
+    }
+}]);
